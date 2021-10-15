f4d644be54a7e12cecf4115836503ac15e7faf4a
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-10-15 23:47:17 +0100

    implement User route and update user route

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-
+//using environmental variables
 dotenv.config();
+const app = express();
 
+//mongoDB connection
 mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log("connected to db");
+    console.log("connected to mongo-db");
   }
 );
 
-const app = express();
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("welcome to homepage");
+});
+app.get("/users", (req, res) => {
+  res.send("welcome to user page");
+});
 
 app.listen(8080, function () {
   console.log("running on 8800");
